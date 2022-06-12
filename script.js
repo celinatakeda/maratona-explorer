b3b35ed72848ccfd9d036b61b143c2ad780116ff
Celina Takeda <takedacarina@gmail.com>
2022-06-12 12:02:25 -0300

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// variaveis?
+// dados de entrada? 
+// dados de saída?
+
+const elementoResposta = document.querySelector('#resposta');
+const inputPergunta = document.querySelector('#inputPergunta');
+const buttonPerguntar = document.querySelector('#buttonPerguntar');
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza",
+  "Não conte com isso",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provalvemente.",
+  "Não é possível prever agora.",
+  "Perspetiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim."
+]
+
+// clicar em fazer pergunta 
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return;
+  }
+
+  buttonPerguntar.setAttribute("disabled", true);
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+// gerar número aleatório
+const totalRespostas = respostas.length;
+const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
+console.log(respostas[numeroAleatorio]);
+
+elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio];
+
+elementoResposta.style.opacity = 1
+
+// sumir a resposta depois de 3 segundos
+setTimeout(function() {
+  elementoResposta.style.opacity = 0;
+  buttonPerguntar.removeAttribute('disabled')
+}, 3000)
+}
+
